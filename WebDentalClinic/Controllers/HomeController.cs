8b9eee397bf922a7d85fddbf25d4fbb84a92fa5c
Hoàng Đệ <hoangde092000@gmail.com>
2021-04-13 12:16:51 +0700

    GiaoDienKhachHang

--- WebDentalClinic/Controllers/HomeController.cs
+++ WebDentalClinic/Controllers/HomeController.cs
@@ -26,5 +26,17 @@ namespace WebDentalClinic.Controllers
 
             return View();
         }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
