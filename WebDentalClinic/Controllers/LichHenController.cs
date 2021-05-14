c313421734065200f80d3d0e0a0b4b0640793e3c
LinhVanQuangNguyen <65953190+QuangLinh158@users.noreply.github.com>
2021-05-14 11:07:58 +0700

    devLinh

--- WebDentalClinic/Controllers/LichHenController.cs
+++ WebDentalClinic/Controllers/LichHenController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -48,5 +49,70 @@ namespace WebDentalClinic.Controllers
         {
             return View();
         }
+        public ActionResult CheckLich(LICHHEN lh)
+        {
+            
+            return View(db.LICHHENs.ToList());
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.LICHHENs.Where(s => s.MaLichHen == id).FirstOrDefault());
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, LICHHEN lh)
+        {
+            try
+            {
+                lh = db.LICHHENs.Where(s => s.MaLichHen == id).FirstOrDefault();
+                db.LICHHENs.Remove(lh);
+                db.SaveChanges();
+                return RedirectToAction("CheckLich");
+            }
+            catch
+            {
+                return Content("Error Delete");
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(db.LICHHENs.Where(s => s.MaLichHen == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, LICHHEN cate)
+        {
+            cate.TinhTrang = "DXN";
+            db.Entry(cate).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Update_Check(LICHHEN lh)
+        {
+            //Check check = Session["Check"] as Check;
+            //int id_lich = int.Parse(form["idLich"]);
+            //string _check = (form["checkl"].ToString());
+            //check.Update_lich(id_lich, _check);
+            //return RedirectToAction("CheckLich", "LichHen");
+
+            lh.TinhTrang = "DXN";
+            db.Entry(lh).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //public Check GetCheck()
+        //{
+        //    Check check = Session["Check"] as Check;
+        //    if(check==null || Session["Check"]==null)
+        //    {
+        //        check = new Check();
+        //        Session["Check"] = check;
+        //    }
+        //    return check;
+        //}
     }
 }
