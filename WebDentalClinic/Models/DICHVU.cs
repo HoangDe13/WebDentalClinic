f0801974e4398f6e0efc3e19ae63403cbec96fe9
nambibobib <73960180+nambibobib@users.noreply.github.com>
2021-05-29 00:44:38 +0700

    Update DICHVU.cs

--- WebDentalClinic/Models/DICHVU.cs
+++ WebDentalClinic/Models/DICHVU.cs
@@ -34,6 +34,5 @@ namespace WebDentalClinic.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CHITIETPHIEUKHAM> CHITIETPHIEUKHAMs { get; set; }
-        public List<DICHVU> listDV { get; internal set; }
     }
 }
